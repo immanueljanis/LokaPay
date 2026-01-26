c8c79011bcb36a4c0359aa7ce1e598a34e0871d2
immanueljanis <immanuelj4nis@gmail.com>
2026-01-26 13:12:25 +0700

    feat(v1): copy sc

--- packages/contracts/contracts/MockUSDT.sol
+++ packages/contracts/contracts/MockUSDT.sol
@@ -5,11 +5,9 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MockUSDT is ERC20 {
     constructor() ERC20("Mock USDT", "mUSDT") {
-        // Cetak 1 Juta Token untuk Deployer
         _mint(msg.sender, 1000000 * 10 ** decimals());
     }
 
-    // Fungsi biar turis bisa minta token gratis (Faucet)
     function mint(address to, uint256 amount) external {
         _mint(to, amount);
     }
